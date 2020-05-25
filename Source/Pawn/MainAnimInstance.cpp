e17c7887128182a95733a92e48f189a788efcdfc
shrma21294 <komalsharma21294@gmail.com>
2020-05-25 19:24:02 -0400

    added delegates

--- Source/Pawn/MainAnimInstance.cpp
+++ Source/Pawn/MainAnimInstance.cpp
@@ -2,4 +2,32 @@
 
 
 #include "MainAnimInstance.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+void UMainAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+
+}
+
+//Custom function which we are calling every frame
+void UMainAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+	}
+
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size(); //return magnitude of vector
+
+		bIsInAir = Pawn->GetMovementComponent()->IsFalling();
+	}
+}
--- Source/Pawn/MainAnimInstance.h
+++ Source/Pawn/MainAnimInstance.h
@@ -13,5 +13,23 @@ UCLASS()
 class PAWN_API UMainAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable, Category = AnimationProperties)
+	void UpdateAnimationProperties();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = Movement)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = Movement)
+	bool bIsInAir;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = Movement)
+	class APawn* Pawn;
 	
+
+
 };
