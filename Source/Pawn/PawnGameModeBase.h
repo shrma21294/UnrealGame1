5a7253874d426731573c2f0af24d6ffa94cf6dee
shrma21294 <komalsharma21294@gmail.com>
2020-05-24 22:05:14 -0400

    Pushing the pawn

---
+++ Source/Pawn/PawnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWN_API APawnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
