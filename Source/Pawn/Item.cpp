d72bf4bb09172fbf50d95fbb75cefab39043fa44
shrma21294 <komalsharma21294@gmail.com>
2020-05-27 01:20:51 -0400

    added pickup and explosive functionality derived from item

--- Source/Pawn/Item.cpp
+++ Source/Pawn/Item.cpp
@@ -35,11 +35,11 @@ void AItem::Tick(float DeltaTime)
 
 void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnOverlapBegin"));
+	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapBegin"));
 }
 
 void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnOverlapEnd"));
+	UE_LOG(LogTemp, Warning, TEXT("Super::OnOverlapEnd"));
 }
 
