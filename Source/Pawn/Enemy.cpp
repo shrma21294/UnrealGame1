4858986aee73235560897abff137fcdcd8bd6217
shrma21294 <komalsharma21294@gmail.com>
2020-05-29 18:53:48 -0400

    added the agro sphere and combat sphere

--- Source/Pawn/Enemy.cpp
+++ Source/Pawn/Enemy.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Enemy.h"
+#include "Components/SphereComponent.h"
+#include "AIController.h"
+#include "Main.h"
 
 // Sets default values
 AEnemy::AEnemy()
@@ -9,12 +12,31 @@ AEnemy::AEnemy()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	AgroSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AgroSphere"));
+	AgroSphere->SetupAttachment(GetRootComponent());
+	AgroSphere->InitSphereRadius(600.f);
+
+	CombatSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CombatSphere"));
+	CombatSphere->SetupAttachment(GetRootComponent());
+	CombatSphere->InitSphereRadius(75.f);
+
+
 }
 
 // Called when the game starts or when spawned
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//to control the Enemy - spider here, we need controller which comes form AI controller class
+	//Here we are casting hte controller to AAIController
+	AIController = Cast<AAIController>(GetController());
+
+	AgroSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::AgroSphereOnOverlapBegin);
+	AgroSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::AgroSphereOnOverlapEnd);
+
+	CombatSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemy::CombatSphereOnOverlapBegin);
+	CombatSphere->OnComponentEndOverlap.AddDynamic(this, &AEnemy::CombatSphereOnOverlapEnd);
 	
 }
 
@@ -31,4 +53,60 @@ void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
+
+
+ void AEnemy::AgroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+ {
+	 if (OtherActor)
+	 {
+		 AMain* Main = Cast<AMain>(OtherActor);
+		 if (Main)
+		 {
+			 MoveToTarget(Main);
+		 }
+	 }
+ }
+
+
+ void AEnemy::AgroSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+ {
+
+ }
+
+
+ void AEnemy::CombatSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+ {
+
+ }
+
+
+ void AEnemy::CombatSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+ {
+
+ }
+
+ //Move the enemy towards the character when come under the colliding sphere
+ //first set the target
+ //then move toward the target
+
+//If not able to recognise AIController - add AIMOdule under Pawn.Build.cs file
+ void AEnemy::MoveToTarget(class AMain* Target)
+ {
+	 SetEnemyMovementStatus(EEnemyMovementStatus::EMS_MoveToTarget);
+	 
+	 if (AIController)
+	 {
+		// UE_LOG(LogTemp, Warning, TEXT("MoveToTarget"));
+		 FAIMoveRequest MoveRequest;
+		 MoveRequest.SetGoalActor(Target);
+		 MoveRequest.SetAcceptanceRadius(5.f);
+
+		 FNavPathSharedPtr NavPath;
+
+		 AIController->MoveTo(MoveRequest, &NavPath);
+
+	 }
+
+ }
+
 
